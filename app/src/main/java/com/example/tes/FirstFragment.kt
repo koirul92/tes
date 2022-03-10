e999762456e0f95a5a8b98a69d0d73db4c10d5e2
koirul92 <koirulanwar98@gmail.com>
2022-03-10 08:53:17 +0700

    add fragment

--- app/src/main/java/com/example/tes/FirstFragment.kt
+++ app/src/main/java/com/example/tes/FirstFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,7 +34,6 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
